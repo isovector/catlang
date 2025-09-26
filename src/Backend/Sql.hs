44cafb212a2d1869c1f4eb97dc442ea9b0b8362b
Sandy Maguire <sandy@sandymaguire.me>
2025-09-26 10:32:15 -0700

    Compile coproducts

--- src/Backend/Sql.hs
+++ src/Backend/Sql.hs
@@ -2,18 +2,51 @@
 
 module Backend.Sql where
 
-import Control.Arrow ((***))
+import Data.Functor
+import Data.Foldable
+import Control.Monad.State
+import Control.Arrow ((***), (&&&))
 import Data.Monoid (Endo(..))
 import Typecheck
 import Text.PrettyPrint.HughesPJClass hiding ((<>), Str)
 import Types
 
+type FieldName = String
+
+data Fields a
+  = FPair (Fields a) (Fields a)
+  | FCopair (Fields a) (Fields a)
+  | Field a
+  deriving stock (Eq, Ord, Show, Functor, Foldable, Traversable)
+
+
+zipF :: Fields a -> Fields b -> Fields (a, b)
+zipF (FPair a1 a2) (FPair b1 b2) = FPair (zipF a1 b1) (zipF a2 b2)
+zipF (FCopair a1 a2) (FCopair b1 b2) = FCopair (zipF a1 b1) (zipF a2 b2)
+zipF (Field a) f = fmap (a,) f
+zipF f (Field b) = fmap (, b) f
+zipF _ _ = error "tried to zip a pair and copair"
+
+toFields :: Type -> Fields ()
+toFields (Prod x y) = FPair (toFields x) (toFields y)
+toFields (Coprod x y) = FCopair (toFields x) (toFields y)
+toFields _ = Field ()
+
+next :: (Enum a, MonadState a m) => m a
+next = get <* modify succ
+
+
+enumerate :: Traversable f => f a -> f FieldName
+enumerate = fmap field . flip evalState 0 . traverse (const next)
+
 
 data Sql
-  = Select [(String, String)] Sql
+  = Select [(FieldName, Maybe FieldName)] Sql
+  | Filter [FieldName] Sql
   | Let Sql Sql
   | LetBound
   | CrossJoin Sql Sql
+  | Union Sql Sql
   | Input
   deriving stock (Eq, Ord, Show)
 
@@ -22,13 +55,22 @@ prettySql (Select fs s) =
   hsep
     [ "SELECT"
     , hsep $ punctuate "," $ do
-        (x, y) <- fs
-        pure $ text x <+> "AS" <+> text y
+        (field, val) <- fs
+        pure $ maybe "NULL" text val <+> "AS" <+> text field
     , "FROM"
     , parens $ prettySql s
     ]
+prettySql (Filter [] s) = prettySql s
+prettySql (Filter ws s) =
+  hsep
+    [ "SELECT *"
+    , "FROM"
+    , parens $ prettySql s
+    , "WHERE"
+    , hsep $ punctuate " AND" $ ws <&> \w -> text w <+> "IS NOT NULL"
+    ]
 prettySql LetBound = "t"
-prettySql Input = "input"
+prettySql Input = "SELECT 1 as f0"
 prettySql (Let x y) =
   vcat
     [ "WITH" <+> "t" <+> "AS"
@@ -42,6 +84,12 @@ prettySql (CrossJoin x y) =
     , "CROSS JOIN"
     , parens $ prettySql y
     ]
+prettySql (Union x y) =
+  vcat
+    [ prettySql x
+    , "UNION"
+    , prettySql y
+    ]
 
 newtype SqlBuilder = SqlBuilder
   { runSqlBuilder :: Sql -> Sql
@@ -49,54 +97,61 @@ newtype SqlBuilder = SqlBuilder
   deriving (Semigroup, Monoid) via Endo Sql
 
 
+field :: Int -> FieldName
+field i = "f" <> show i
 
 
-sizeCount :: Type -> Int
-sizeCount (Prod x y) = sizeCount x + sizeCount y
-sizeCount _ = 1
+example :: Expr ()
+example = AndThen Inr (Join Inr Inl)
 
-field :: Int -> String
-field i = "p" <> show i
-
-move' :: Int -> Int -> Int -> SqlBuilder
-move' out_off offset size
-  = SqlBuilder
-  $ Select
-  $ fmap (field *** field)
-  $ zip [offset..] [out_off..(out_off + size - 1)]
-
-move :: Int -> Int -> SqlBuilder
-move = move' 0
+exampleS :: With (Expr ()) Type
+Right exampleS = runTcM $ infer example
 
 
--- example :: Expr ()
--- example = Fork Proj2 Proj1
+toCanonical :: Fields FieldName -> SqlBuilder
+toCanonical fs = SqlBuilder $ Select $ do
+  let l = toList fs
+  zip (enumerate l) $ fmap Just l
 
--- exampleS :: With (Expr ()) Type
--- Right exampleS = runTcM $ infer example
+fromCanonical :: Fields FieldName -> SqlBuilder
+fromCanonical fs = SqlBuilder $ Select $ do
+  let l = toList fs
+  zip l $ fmap Just $ enumerate l
 
 
 sqlAlg :: Type -> ExprF b SqlBuilder -> SqlBuilder
-sqlAlg (Arr (Prod x _) _) Proj1F = move 0 (sizeCount x)
+sqlAlg _ IdF = mempty
+sqlAlg (Arr ty _) Proj1F
+  | FPair p1 _ <- enumerate $ toFields ty
+  = toCanonical p1
 sqlAlg _ Proj1F = error "bad type"
-sqlAlg (Arr (Prod x y) _) Proj2F = move (sizeCount x) (sizeCount y)
+sqlAlg (Arr ty _) Proj2F
+  | FPair _ p2 <- enumerate $ toFields ty
+  = toCanonical p2
 sqlAlg _ Proj2F = error "bad type"
-sqlAlg _ (AndThenF f g) = f <> g
-sqlAlg (Arr _ (Prod x y)) (ForkF f g) =
+sqlAlg _ (AndThenF f g) = g <> f
+sqlAlg (Arr _ (enumerate . toFields -> FPair x y)) (ForkF f g) =
   SqlBuilder $ \input ->
-    Let input $ CrossJoin (runSqlBuilder f LetBound) (runSqlBuilder (move' (sizeCount x) 0 (sizeCount y) <> g) LetBound)
+    Let input $
+      CrossJoin
+        (runSqlBuilder (fromCanonical x <> f) LetBound)
+        (runSqlBuilder (fromCanonical y <> g) LetBound)
 sqlAlg _ ForkF{} = error "bad type"
+sqlAlg (Arr _ (enumerate . toFields -> FCopair x y)) InlF =
+  SqlBuilder $ Select $ toList $ FCopair (fmap (id &&& Just) x) (fmap (, Nothing) y)
+sqlAlg _ InlF = error "bad inl"
+sqlAlg (Arr _ (enumerate . toFields -> FCopair x y)) InrF =
+  let y0 = enumerate y
+   in SqlBuilder $ Select $ toList $ FCopair (fmap (, Nothing) x) (zipF y $ fmap Just y0)
+sqlAlg _ InrF = error "bad inr"
+sqlAlg (Arr (enumerate . toFields -> FCopair x y) _) (JoinF f g) =
+  SqlBuilder $ \sql ->
+    Let sql $
+      Union
+        (runSqlBuilder (f <> toCanonical x) $ Filter (toList x) LetBound)
+        (runSqlBuilder (g <> toCanonical y) $ Filter (toList y) LetBound)
+sqlAlg _ JoinF{} = error "bad join"
 
--- --     Proj2F
--- --       | Arr (Prod x y) _ <- ty
--- --       -> pure $ move (sizeCount x) (sizeCount y)
--- --     AndThenF x y ->
--- --       pure $ unlines
--- --         [ "SELECT"
--- --         , y
--- --         , "FROM ("
--- --         , x
--- --         , ")"
--- --         ]
--- --     ForkF x y -> _
+
+
 
